fa79c1df8ca9fde086ab38c84475e5ea7637467d
hailey8 <1258442848@qq.com>
2019-05-18 14:42:25 +0800

    新建类别控制器

---
+++ HST.Art.Web/Areas/manage/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HST.Art.Web.Areas.manage.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: manage/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
