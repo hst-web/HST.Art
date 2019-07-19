529fca0b672bf261b0532a7f98c15191ededc459
sysmenu <873292778@qq.com>
2019-07-19 19:46:39 +0800

    初始化数据提交

--- HST.Art.Core/DataBase/初始化数据.sql
+++ HST.Art.Core/DataBase/初始化数据.sql
@@ -1,4 +1,6 @@
 insert into [user] (UserName, Password, Salt, Name, Email, Telephone, HeadImg, IsAdmin, State) values ('admin','WAvPHVB3mX8=','dQBHcZJa','管理员','','','',1,1)
-insert into [Organization] (Number) values ('Z190001')
-insert into [Setting] ([Val],[Type]) values ('ejEKu8VnIRtJy04khJROEW12X7NAohrnMqOxuKUN0ygnOKLyN5J7MsCNO2uA5xkentygk5o9bBk=',1)
+insert into [Organization] (Name,Number) values ('山东省艺术职业教育专业建设指导委员会','Z190001')
+insert into [Setting] ([Val],[Type]) values ('ejEKu8VnIRtJy04khJROEQgOl7G+/J1LEmGnrK93K04nQZi5nTKzA3z05oZu13QWAGSgO02Y6RymakKVxX4pfg==',1)
 insert into [Setting] ([Val],[Type]) values ('',2)
+
+--ejEKu8VnIRtJy04khJROEQgOl7G+/J1LEmGnrK93K04nQZi5nTKzA3z05oZu13QWnSj/UNSQy4w=
